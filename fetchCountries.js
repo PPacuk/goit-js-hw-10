f2bcc16c110cdd9b5dc243fa851266d33cbdc631
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-03-27 14:23:30 +0200

    fetch

--- fetchCountries.js
+++ fetchCountries.js
@@ -1,8 +1,8 @@
 export { fetchCountries };
 const fetchCountries = country => {
   const url = `https://restcountries.com/v3.1/name/${country}`;
-    const countryInfo = document.querySelector('.country-info');
-    const countryList = document.querySelector('.country-list');
+  const countryInfo = document.querySelector('.country-info');
+  const countryList = document.querySelector('.country-list');
   fetch(url)
     .then(resp => {
       if (!resp.ok) {
@@ -10,18 +10,22 @@ const fetchCountries = country => {
       }
       return resp.json();
     })
-      .then(data => {
-        //  data.map(e => console.log(Object.values(e.name)));
-          
-          data.map(e => {
-            //   const countryEl = document.createElement('li');
-              data.map(e => console.log(e.name.common))
-            //   countryList.innerHTML = `
-            // <ul><li><img src='${e.flags.svg}', height="20px", border='1px'> ${e.name.official
-            //       }</li></ul>`;
-              
+    .then(data => {
+      const countryNumber = data.map(e => e.name.official).length;
+// console.log(countryNumber);
+      if (countryNumber > 10) {
+        
+      } else {
+        data.map(e => {
+          const countryLiEl = `<li><img src='${e.flags.svg}', height="20px", border='1px'> ${e.name.official}</li>`;
+            //  countryList.innerHTML = countryLiEl.repeat(countryNumber);
+          console.log(countryLiEl);
+          const setCountryList = (countryNumber, countryLiEl) => {
+            return countryList.innerHTML = countryNumber + countryLiEl
+          }
+return setCountryList(countryNumber, countryLiEl);
         });
-          
+      }
       data.map(e => {
         const lang = e.languages;
 
@@ -33,8 +37,8 @@ const fetchCountries = country => {
             <li>${e.population}</li>
             <li>${Object.values(lang)}</li></ul>
             `;
-      })}
-    )
+      });
+    })
 
     .catch(console.error);
 };
